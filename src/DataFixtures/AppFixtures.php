ab877f75aafe8d685533722cd0fbd7cc2ef4889f
Luca DUPONT <luca.dupont@orange.fr>
2023-03-06 15:48:20 +0100

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,18 +2,97 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Lieu;
+use App\Entity\User;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    )
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        $this->addVilles(20);
+        //$this->addLieux(50);
+        //$this->addUsers(20);
     }
+
+    private function addVilles(int $number = 10)
+    {
+        for ($i = 0; $i < $number; $i++) {
+            $ville = new Ville();
+            $ville
+                ->setNom($this->faker->city)
+                ->setCodePostal($this->faker->postcode);
+
+            $this->entityManager->persist($ville);
+        }
+        $this->entityManager->flush();
+    }
+
+    private function addLieux(int $number = 10)
+    {
+        for ($i = 0; $i < $number; $i++) {
+            $lieu = new Lieu();
+            $lieu
+                ->setNom($this->faker->title)
+                ->setRue($this->faker->address)
+                ->setLatitude($this->faker->latitude)
+                ->setLongitude($this->faker->longitude)
+                ->setVille($this->entityManager->find(Ville::class, $this->faker->numberBetween(1,20)));
+            $this->entityManager->persist($lieu);
+        }
+        $this->entityManager->flush();
+    }
+
+    private function addUsers(int $number = 10)
+    {
+
+        $admin = new User();
+        $admin->setNom('Admin')
+            ->setPrenom('Admin')
+            ->setPseudo('Admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setTelephone('0203040506')
+            ->setActif(true)
+            ->setImage('image.png');
+
+        $password = $this->passwordHasher->hashPassword($admin, 'admin');
+        $admin->setPassword($password);
+
+
+        for ($i = 0; $i < $number; $i++) {
+            $user = new User();
+            $user->setNom($this->faker->lastName)
+                ->setPrenom($this->faker->firstName)
+                ->setPseudo($this->faker->userName)
+                ->setRoles(['ROLE_USER'])
+                ->setTelephone($this->faker->phoneNumber)
+                ->setActif($this->faker->boolean())
+                ->setImage('image.png');
+
+                $password = $this->passwordHasher->hashPassword($user, '123');
+                $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+        }
+        $this->entityManager->flush();
+    }
+
 
 
 }
