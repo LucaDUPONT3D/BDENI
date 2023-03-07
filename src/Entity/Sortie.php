63af930b1c4c3d63d1df0e8c575b8a4cf38aaef2
Luca DUPONT <luca.dupont@orange.fr>
2023-03-07 16:28:33 +0100

    Board Functionnalities

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SortieRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Sortie
 {
     #[ORM\Id]
@@ -30,8 +31,6 @@ class Sortie
 
     #[ORM\Column(nullable: true)]
     private ?int $nbInsriptionsMax = null;
-
-    private ?int $nbParticipants = 0;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $infosSortie = null;
@@ -173,59 +172,41 @@ class Sortie
         return $this;
     }
 
-public function getOrganisateur(): ?User
-{
-    return $this->organisateur;
-}
-
-public function setOrganisateur(?User $organisateur): self
-{
-    $this->organisateur = $organisateur;
-
-    return $this;
-}
-
-/**
- * @return Collection<int, User>
- */
-public function getParticipants(): Collection
-{
-    return $this->participants;
-}
-
-public function addParticipant(User $participant): self
-{
-    if (!$this->participants->contains($participant)) {
-        $this->participants->add($participant);
-        $this->nbParticipants += 1;
+    public function getOrganisateur(): ?User
+    {
+        return $this->organisateur;
     }
 
-    return $this;
-}
-
-public function removeParticipant(User $participant): self
-{
-    $this->participants->removeElement($participant);
-    $this->nbParticipants -= 1;
-
-    return $this;
-}
-
-    /**
-     * @return int|null
-     */
-    public function getNbParticipants(): ?int
+    public function setOrganisateur(?User $organisateur): self
     {
-        return $this->nbParticipants;
-    }
+        $this->organisateur = $organisateur;
 
-    /**
-     * @param int|null $nbParticipants
-     */
-    public function setNbParticipants(?int $nbParticipants): self
-    {
-        $this->nbParticipants = $nbParticipants;
         return $this;
     }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(User $participant): self
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(User $participant): self
+    {
+        $this->participants->removeElement($participant);
+
+        return $this;
+    }
+
 
 }
