653b71895172287f1744fa51ceffac13553e11d5
mrescamp2022 <maxime.rescamp@hotmail.fr>
2023-03-07 10:36:54 +0100

    ajout du formulaire

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,20 +2,23 @@
 
 namespace App\Controller;
 
+use App\Form\FiltreType;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/sortie', name: 'sortie_')]
 class SortieController extends AbstractController
 {
     #[Route('/', name: 'all')]
     public function afficher(SortieRepository $sortieRepository): Response
     {
-       $sortie = $sortieRepository->findALLjoin();
-
+        $formFiltre = $this->createForm(FiltreType::class);
+        $sortie = $sortieRepository->findALLjoin();
         return $this->render('sortie/afficher.html.twig', [
-            'sorties' => $sortie
+            'sorties' => $sortie,
+            'form' => $formFiltre->createView()
         ]);
     }
 }
---
+++ src/Form/FiltreType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FiltreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Campus', ChoiceType::class, [
+                'choices' => [
+                    'Rennes' => 'Rennes',
+                    'Quimper' => 'Quimper',
+                    'Nantes' => 'Nantes',
+                    'Niort' => 'Niort',
+                    'Angers' => 'Angers'
+                ],
+                'label'=> 'Campus',
+                'multiple' => false,
+                'expanded' => false
+            ])
+
+        ->add('recherche', SearchType::class,[
+        'label'=>'le nom de la sortie contient : '])
+
+            ->add('entre', DateType::class,[
+                'label'=> 'Entre : ',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('et', DateType::class,[
+                'label'=> 'et : ',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('critere', ChoiceType::class, [
+                'choices' => [
+                    'Sortie dont je suis organisateur' => 'Sortie dont je suis organisateur',
+                    'Sortie au quel je suis inscrit' => 'Sortie au quel je suis inscrit',
+                    'Sortie au quel je ne suis pas inscrit' => 'Sortie au quel je ne suis pas inscrit',
+                    'Sortie Passé' => 'Sortie Passé',
+                ],
+                'multiple' => true,
+                'expanded' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
