be844eec0b6a54e3dab966709fb4376b527d0c7a
mrescamp2022 <maxime.rescamp@hotmail.fr>
2023-03-09 14:55:36 +0100

    ajout des asset utilisateur et sortie

--- src/Entity/Etat.php
+++ src/Entity/Etat.php
@@ -6,6 +6,7 @@ use App\Repository\EtatRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: EtatRepository::class)]
 class Etat
