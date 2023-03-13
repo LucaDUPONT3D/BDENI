0e3828c255adf436be7b14c8f47b6880df91d4c0
mrescamp2022 <maxime.rescamp@hotmail.fr>
2023-03-13 14:38:47 +0100

    comit

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -7,6 +7,7 @@ use App\Entity\Ville;
 use App\Repository\VilleRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +16,10 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('rue')
-            ->add('latitude')
-            ->add('longitude')
+            ->add('nom', TextType::class, ['label'=>'Nom : ','attr' => ['placeholder' => 'Entrez un nom ...']])
+            ->add('rue', TextType::class, ['label'=>'Adresse : ','attr' => ['placeholder' => 'Entrez une adresse ...']])
+            ->add('latitude', TextType::class, ['label'=>'Latitude : ','attr' => ['placeholder' => 'Entrez une latitude ...']])
+            ->add('longitude', TextType::class, ['label'=>'Longitude : ','attr' => ['placeholder' => 'Entrez une longitude ...']])
             ->add(
                 'ville',
                 EntityType::class,
--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class VilleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('codePostal')
+            ->add('nom', TextType::class, ['label'=>'Nom : ','attr' => ['placeholder' => 'Entrez un nom ...']])
+            ->add('codePostal', TextType::class, ['label'=>'Code Postal : ','attr' => ['placeholder' => 'Entrez un code postal ...']])
         ;
     }
 
