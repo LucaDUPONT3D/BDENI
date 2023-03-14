3436800b334cc168cb51f867fefe98a6ead1fe8b
Ewen357 <ewen.malaquin@gmail.com>
2023-03-14 12:05:28 +0100

    CSS + Script tableau

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('#tab-sorties').DataTable();
+
+    let element = $('#tab-sorties_filter,#tab-sorties_length,#tab-sorties_info,#tab-sorties_paginate');
+
+    element.hide() ;
+
+});
+
