f3338a436f0b70662182cd2ee1b448f99984581c
Luca DUPONT <luca.dupont@orange.fr>
2023-03-14 17:44:46 +0100

    CSS Mardi Soir 3

--- src/Form/CampusType.php
+++ src/Form/CampusType.php
@@ -13,7 +13,7 @@ class CampusType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class, ['label'=>'Nom : ','attr' => ['placeholder' => 'Entrez un nom ...']])
+            ->add('nom', TextType::class, ['label'=>'Nom : ','attr' => ['placeholder' => 'Entrez un nom ...', 'class'=> 'form-control']])
         ;
     }
 
